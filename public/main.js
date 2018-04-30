416862d960ef42515dad69dab0c50500e719b1cf
Danielle Danskin <danielle.danskin@gmail.com>
2018-04-29 23:58:02 -0400

    add faq

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $('.collapsible').collapsible();
     $('select').formSelect();
+    $('#search_type').on('change', function() {
+      $('#result_type').val($('#search_type').val());
+    });
   });
-        
