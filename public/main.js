49433c27550483b9b3ffa7e61820b5d54c12d909
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-03 12:08:06 -0400

    fix nav

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".button-collapse").sidenav();
+    $(".button-collapse").side-nav();
     $('.collapsible').collapsible();
     $('select').formSelect();
     $('#search_type').on('change', function() {
