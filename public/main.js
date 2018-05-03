8b703afa7d9cbfd764abb6269da18901c226e914
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-03 09:08:06 -0400

    fix nav

--- public/main.js
+++ public/main.js
@@ -1,25 +1,8 @@
 $(document).ready(function(){
-
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
+    $('select').formSelect();
     $('#search_type').on('change', function() {
       $('#result_type').val($('#search_type').val());
     });
-
-});
-
-// nav bar mobile-responsive function
-document.addEventListener('DOMContentLoaded' function() {
-     var elems = document.querySelectorAll('.sidenav');
-     var instances = M.Sidenav.init(elems, options);
-});
-
-// selector function
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-});
-
-// collapsible function
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
 });
